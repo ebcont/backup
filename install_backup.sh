1339c176b43d1a55a1a751854a20893b2b1a1d51
Christopher Anderlik <christopher.anderlik@gmail.com>
2013-10-15 08:50:39 +0200

    added cronjob for git-update

--- install_backup.sh
+++ install_backup.sh
@@ -1,6 +1,8 @@
 #install backup
 
 apt-get install git
+#git-core on ubuntu 10.04
+
 mkdir -p /root/bin/git/backup
 cd /root/bin/git/backup
 git init
@@ -9,4 +11,6 @@ git --work-tree=/root/bin/git/backup/ --git-dir=/root/bin/git/backup/.git/ pull 
 cp /root/bin/git/backup/credentials /root/bin/
 chmod 700 /root/bin/credentials
 
+
+echo "10 0 * * *     root     git --work-tree=/root/bin/git/backup/ --git-dir=/root/bin/git/backup/.git/ pull https://github.com/ebcont/backup.git" >> /etc/crontab
 
